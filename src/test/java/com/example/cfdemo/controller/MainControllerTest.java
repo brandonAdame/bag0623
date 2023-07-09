2ce1803cb47355156728effbd63a06fbe35bad4b
Brandon Adame <badamegachuz@gmail.com>
2023-07-09 12:23:51 -0400

    Improving unit tests according to spec

--- src/test/java/com/example/cfdemo/controller/MainControllerTest.java
+++ src/test/java/com/example/cfdemo/controller/MainControllerTest.java
@@ -8,18 +8,21 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -39,16 +42,16 @@ public class MainControllerTest {
     private Tool ladder;
 
     private Tool chainsaw;
+    private Tool jackhammerRigid;
+    private Tool jackhammerDewalt;
 
-    @MockBean
-    private NumberFormat numberFormat;
+    private final NumberFormat numberFormat = NumberFormat.getCurrencyInstance(Locale.US);
 
-    @MockBean
-    private DateTimeFormatter dateTimeFormatter;
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yy");;
 
     @BeforeEach
     void setup() {
-        Tool jackhammerRigid = new Tool();
+        jackhammerRigid = new Tool();
         jackhammerRigid.setTool_code("JAKR");
         jackhammerRigid.setTool_type("Jackhammer");
         jackhammerRigid.setBrand("Rigid");
@@ -57,7 +60,7 @@ public class MainControllerTest {
         jackhammerRigid.setWeekend_charge(false);
         jackhammerRigid.setHoliday_charge(false);
 
-        Tool jackhammerDewalt = new Tool();
+        jackhammerDewalt = new Tool();
         jackhammerDewalt.setTool_code("JAKD");
         jackhammerDewalt.setTool_type("Jackhammer");
         jackhammerDewalt.setBrand("DeWalt");
@@ -86,6 +89,7 @@ public class MainControllerTest {
     }
 
     @Test
+    @DisplayName("Jackhammer Bad Request DiscountPercent")
     void checkoutJackhammer_BadRequest_DiscountPercent() throws Exception {
         // Given
         Checkout request = new Checkout();
@@ -99,10 +103,12 @@ public class MainControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(asJsonString(request)))
                 .andDo(print())
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.response").value("discountPercent: not in the range 0-100"));
     }
 
     @Test
+    @DisplayName("Checkout Ladder")
     void testCheckoutLadder() throws Exception {
         // Given
         Checkout request = new Checkout();
@@ -134,10 +140,16 @@ public class MainControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(asJsonString(request)))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.toolCode").value("LADW"))
+                .andExpect(jsonPath("$.rentalDays").value(3))
+                .andExpect(jsonPath("$.discountPercent").value("10%"))
+                .andExpect(jsonPath("$.dueDate").value("07/05/20"))
+                .andExpect(jsonPath("$.finalCharge").value("$5.37"));
     }
 
     @Test
+    @DisplayName("Checkout Chainsaw")
     void testCheckoutChainsaw() throws Exception {
         // Given
         Checkout request = new Checkout();
@@ -169,10 +181,16 @@ public class MainControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(asJsonString(request)))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.toolCode").value("CHNS"))
+                .andExpect(jsonPath("$.rentalDays").value(5))
+                .andExpect(jsonPath("$.finalCharge").value("$3.35"))
+                .andExpect(jsonPath("$.discountPercent").value("25%"))
+                .andExpect(jsonPath("$.dueDate").value("07/07/15"));
     }
 
     @Test
+    @DisplayName("Checkout Jackhammer DeWalt")
     public void testCheckoutJackhammer_DeWalt() throws Exception {
         // Given
         Checkout request = new Checkout();
@@ -182,13 +200,13 @@ public class MainControllerTest {
         request.setCheckoutDate(LocalDate.of(2015, 9, 3));
 
         RentalAgreement expectedAgreement = RentalAgreement.builder()
-                .toolCode(chainsaw.getTool_code())
-                .toolType(chainsaw.getTool_type())
-                .toolBrand(chainsaw.getBrand())
+                .toolCode(jackhammerDewalt.getTool_code())
+                .toolType(jackhammerDewalt.getTool_type())
+                .toolBrand(jackhammerDewalt.getBrand())
                 .rentalDays(request.getRentalDayCount())
                 .checkoutDate(request.getCheckoutDate().format(dateTimeFormatter))
                 .dueDate(LocalDate.of(2015, 9, 9).format(dateTimeFormatter))
-                .dailyRentalCharge(numberFormat.format(chainsaw.getDaily_charge()))
+                .dailyRentalCharge(numberFormat.format(jackhammerDewalt.getDaily_charge()))
                 .chargeDays(4)
                 .preDiscountCharge(numberFormat.format(new BigDecimal("11.96")))
                 .discountPercent("0%")
@@ -204,10 +222,16 @@ public class MainControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(asJsonString(request)))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.toolCode").value("JAKD"))
+                .andExpect(jsonPath("$.rentalDays").value(6))
+                .andExpect(jsonPath("$.discountPercent").value("0%"))
+                .andExpect(jsonPath("$.dueDate").value("09/09/15"))
+                .andExpect(jsonPath("$.finalCharge").value("$11.96"));
     }
 
     @Test
+    @DisplayName("Checkout Jackhammer Ridgid")
     public void testCheckoutJackhammer_Ridgid() throws Exception {
         // Given
         Checkout request = new Checkout();
@@ -217,13 +241,13 @@ public class MainControllerTest {
         request.setCheckoutDate(LocalDate.of(2015, 7, 2));
 
         RentalAgreement expectedAgreement = RentalAgreement.builder()
-                .toolCode(chainsaw.getTool_code())
-                .toolType(chainsaw.getTool_type())
-                .toolBrand(chainsaw.getBrand())
+                .toolCode(jackhammerRigid.getTool_code())
+                .toolType(jackhammerRigid.getTool_type())
+                .toolBrand(jackhammerRigid.getBrand())
                 .rentalDays(request.getRentalDayCount())
                 .checkoutDate(request.getCheckoutDate().format(dateTimeFormatter))
                 .dueDate(LocalDate.of(2015, 7, 11).format(dateTimeFormatter))
-                .dailyRentalCharge(numberFormat.format(chainsaw.getDaily_charge()))
+                .dailyRentalCharge(numberFormat.format(jackhammerRigid.getDaily_charge()))
                 .chargeDays(6)
                 .preDiscountCharge(numberFormat.format(new BigDecimal("17.94")))
                 .discountPercent("0%")
@@ -239,10 +263,16 @@ public class MainControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(asJsonString(request)))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.toolCode").value("JAKR"))
+                .andExpect(jsonPath("$.rentalDays").value(9))
+                .andExpect(jsonPath("$.discountPercent").value("0%"))
+                .andExpect(jsonPath("$.dueDate").value("07/11/15"))
+                .andExpect(jsonPath("$.finalCharge").value("$17.94"));
     }
 
     @Test
+    @DisplayName("Checkout Ridgid Fifty Percent Off")
     public void testCheckoutJackhammer_Ridgid_FiftyPercentOff() throws Exception {
         // Given
         Checkout request = new Checkout();
@@ -252,13 +282,13 @@ public class MainControllerTest {
         request.setCheckoutDate(LocalDate.of(2020, 7, 2));
 
         RentalAgreement expectedAgreement = RentalAgreement.builder()
-                .toolCode(chainsaw.getTool_code())
-                .toolType(chainsaw.getTool_type())
-                .toolBrand(chainsaw.getBrand())
+                .toolCode(jackhammerRigid.getTool_code())
+                .toolType(jackhammerRigid.getTool_type())
+                .toolBrand(jackhammerRigid.getBrand())
                 .rentalDays(request.getRentalDayCount())
                 .checkoutDate(request.getCheckoutDate().format(dateTimeFormatter))
                 .dueDate(LocalDate.of(2020, 7, 6).format(dateTimeFormatter))
-                .dailyRentalCharge(numberFormat.format(chainsaw.getDaily_charge()))
+                .dailyRentalCharge(numberFormat.format(jackhammerRigid.getDaily_charge()))
                 .chargeDays(2)
                 .preDiscountCharge(numberFormat.format(new BigDecimal("5.98")))
                 .discountPercent("50%")
@@ -274,10 +304,16 @@ public class MainControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(asJsonString(request)))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.toolCode").value("JAKR"))
+                .andExpect(jsonPath("$.rentalDays").value(4))
+                .andExpect(jsonPath("$.discountPercent").value("50%"))
+                .andExpect(jsonPath("$.dueDate").value("07/06/20"))
+                .andExpect(jsonPath("$.finalCharge").value("$2.99"));
     }
 
     @Test
+    @DisplayName("Checkout Jackhammer BadRequest RentalDayCount")
     void checkoutJackhammer_BadRequest_RentalDayCount() throws Exception {
         // Given
         Checkout request = new Checkout();
